33d473f65f4e61ca377acfcc0d5c3168260ef559
Alexis Lazcano <0370102@cegepmontpetit.ca>
2022-10-17 17:35:51 -0400

    FCT: Activité d'accueil (-menu) et création d'une question

--- app/src/main/java/com/example/a21_4203n5_votodroid_lazcano/MainActivity.java
+++ app/src/main/java/com/example/a21_4203n5_votodroid_lazcano/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.a21_4203n5_votodroid_lazcano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.a21_4203n5_votodroid_lazcano.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.buttonAjouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(getApplicationContext(), AddQuestionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
